cdbcc99dc3e2051db184dfff31d14a7913128e1c
Chris Reddish <133260588+Cred1747@users.noreply.github.com>
2025-05-03 21:54:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -155,4 +155,5 @@ def update_graph(model, kval, clickData):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
+
